a5d4d97e7fa711be4ee8a48bb684cb2ad4b462c8
arslaan khan <arslaank920@outlook.com>
2023-02-28 19:44:57 +0530

    created web app for nayabazaar

---
+++ NAYABAZAARCLOUD/NAYABAZAARCLOUD/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using NAYABAZAARCLOUD.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NAYABAZAARCLOUD.Controllers
+{
+    
+    public class HomeController : Controller
+    {
+
+        private ProductRepository productRepository;
+        public HomeController()
+        {
+            productRepository = new ProductRepository();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(productRepository.GetProducts());
+        }
+    }
+}
---
+++ NAYABAZAARCLOUD/NAYABAZAARCLOUD/Repository/ProductRepository.cs
@@ -0,0 +1,57 @@
+﻿using NAYABAZAARCLOUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace NAYABAZAARCLOUD.Services
+{
+    public class ProductRepository
+    {
+        private static string db_source = "nayabazaar.database.windows.net";
+        private static string db_user = "Arslaan123";
+        private static string db_password = "123Arslaank@";
+        private static string db_database = "nayabazaar";
+
+        private SqlConnection GetConnection()
+        {
+            var _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = db_source;
+            _builder.UserID = db_user;
+            _builder.Password = db_password;
+            _builder.InitialCatalog = db_database;
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Products> GetProducts()
+        {
+            SqlConnection conn = GetConnection();
+
+            List<Products> products_lst = new List<Products>();
+
+            string statement = "SELECT * FROM products";
+
+            conn.Open();
+
+            SqlCommand cmd = new SqlCommand(statement, conn);
+
+            using(SqlDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Products product = new Products()
+                    {
+                        ProductName = reader.GetString(0),
+                        Price = reader.GetInt32(1)
+
+                    };
+                    products_lst.Add(product);
+                }
+
+            }
+            conn.Close();
+            return products_lst;
+        }
+    }
+}
